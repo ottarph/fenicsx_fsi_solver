9014353986c24b4d864bc9c1e03c5d15c78fa582
Ottar Hellan <hellanottar@simula.no>
2024-11-18 20:00:56 +0100

    Attempt at DFG2d-3 using crank-nicolson navierstokes solver for verification.

---
+++ dfg_2d_3.py
@@ -0,0 +1,375 @@
+import dolfinx as dfx
+import dolfinx.fem.petsc as dfpetsc
+import numpy as np
+import basix.ufl
+import ufl
+from petsc4py import PETSc
+
+import sys
+from os import PathLike
+
+from matplotlib import pyplot as plt
+
+from mpi4py import MPI
+from mpi4py.MPI import COMM_WORLD as comm
+
+PHYSICAL_MARKERS = {
+    "solid": 1,
+    "ALE_fluid": 2,
+
+    "solid_fluid_interface": 11,
+
+    "obstacle": 21,                 # no-slip for fluid
+    "inflow": 22,                   # parabolic inflow for fluid
+    "outflow": 23,                  # do-nothing for fluid
+    "channel_side": 24,             # no-slip for fluid
+}
+
+def main():
+
+    log = PETSc.Log()
+    log.begin()
+
+    # load mesh and meshtags
+
+    mesh_path = "data/meshes/dfg2d/mesh.xdmf"
+
+    with dfx.io.XDMFFile(comm, mesh_path, "r") as infile:
+        fluid_mesh = infile.read_mesh()
+        cell_tags = infile.read_meshtags(fluid_mesh, name= "Cell tags")
+        fluid_mesh.topology.create_connectivity(1, 2)
+        fluid_facet_tags = infile.read_meshtags(fluid_mesh, name= "Facet tags")
+
+    assert len(np.setdiff1d(np.union1d(cell_tags.values, fluid_facet_tags.values), [PHYSICAL_MARKERS[i] for i in PHYSICAL_MARKERS])) == 0, "Physical markers and cell tags do not match"
+    
+
+    if comm.rank == 0:
+        print(f"{fluid_mesh.geometry.x.shape = }")
+
+
+    # Create measure with  meshtags
+
+    dx = ufl.Measure("dx", domain=fluid_mesh)
+    ds = ufl.Measure("ds", domain=fluid_mesh, subdomain_data=fluid_facet_tags)
+
+
+    # create problem parameters
+
+    rho_f = dfx.fem.Constant(fluid_mesh, 1.0)
+    mu_val = 1e-3
+    nu_f = dfx.fem.Constant(fluid_mesh, mu_val / rho_f.value)
+
+    U_bar = 1.0
+    H = 0.41
+
+    t0 = 0.0
+    T = 4.0
+    # T = 8.0                    # Final time
+    # dt_val = 1 / 1600
+    dt_val = 1 / 400
+    dt = dfx.fem.Constant(fluid_mesh, dt_val)
+
+    theta = dfx.fem.Constant(fluid_mesh, 0.5)
+
+    save_every = 10
+
+    
+    # create function spaces
+
+    V = dfx.fem.functionspace(fluid_mesh, ("CG", 2, (2, )))
+    P = dfx.fem.functionspace(fluid_mesh, ("CG", 1))
+    W = ufl.MixedFunctionSpace(V, P)
+
+
+    # create functions
+
+    v, p = dfx.fem.Function(V, name="v"), dfx.fem.Function(P, name="p")
+    v_old = dfx.fem.Function(V)
+
+    
+    # Crank-Nicolson discretization of Navier-Stokes, with pressure treated fully implicitly
+    # Parabolic inflow on left side, no-slip on top, bottom, obstacle, and flag, do-nothing on right side
+    
+    dv_dt = (v - v_old) / dt
+
+    v_theta = theta * v + (1.0 - theta) * v_old
+    
+    dv, dp = ufl.TestFunctions(W)
+    delta_v, delta_p = ufl.TrialFunctions(W)
+
+
+    # Eulerian formulation of transient Navier-Stokes
+    # Parabolic inflow on left side, no-slip on top, bottom, obstacle, and flag, do-nothing on right side
+    
+    from fsi.materials import Fluid
+
+    n = ufl.FacetNormal(fluid_mesh)
+    
+
+    # create Dirichlet boundary condition
+
+    from functools import reduce
+
+    bc_func = dfx.fem.Function(V)
+    bc_func.x.array[:] = 0.0
+    bc_facets = reduce(np.union1d, [
+        fluid_facet_tags.find(PHYSICAL_MARKERS["obstacle"]),
+        fluid_facet_tags.find(PHYSICAL_MARKERS["inflow"]),
+        fluid_facet_tags.find(PHYSICAL_MARKERS["channel_side"]),
+        ])
+    bc_dofs = dfx.fem.locate_dofs_topological(V, fluid_mesh.geometry.dim - 1, bc_facets)
+
+    class BCFunc:
+        def __init__(self, t: float = 0.0):
+            self.t = t
+        def __call__(self, x: np.ndarray) -> np.ndarray:
+            values = np.zeros((2, x.shape[1]), dtype=x.dtype)
+            values[0] = np.where(np.isclose(x[0], 0.0), 1.5 * U_bar * 4 * x[1] * (H - x[1]) / H**2, 0.0)
+            values[0] *= np.sin(self.t * np.pi / 8)
+            return values
+    bc_func.interpolate(BCFunc(t0))
+
+    bc = dfx.fem.dirichletbc(bc_func, bc_dofs)
+
+    bcs = [bc]
+
+
+    # create residual form
+
+    residual = rho_f * ufl.inner(dv_dt + ufl.dot(v_theta, ufl.grad(v_theta)), dv) * dx
+    residual += ufl.inner(Fluid.NS_velocity_eulerian(v_theta, nu_f, rho_f), ufl.grad(dv)) * dx
+    residual += ufl.inner(Fluid.NS_pressure(p), ufl.grad(dv)) * dx
+
+    residual += ufl.div(v) * dp * dx
+
+    # Do-nothing condition
+    residual -= rho_f * nu_f * ufl.inner(ufl.grad(v).T * n, dv) * ds(PHYSICAL_MARKERS["outflow"])
+
+
+    residual_blocked = ufl.extract_blocks(residual)
+    residual_comp = dfx.fem.form(residual_blocked)
+
+    
+    # create Jacobian form
+
+    jacobian = ufl.derivative(residual, v, delta_v) + ufl.derivative(residual, p, delta_p)
+    jacobian_blocked = ufl.extract_blocks(jacobian)
+    jacobian_comp = dfx.fem.form(jacobian_blocked)
+
+
+    # create matrix and vector for linear algebra
+
+    A = dfpetsc.create_matrix_block(jacobian_comp)
+    b = dfpetsc.create_vector_block(residual_comp)
+    x = dfpetsc.create_vector_block(residual_comp)
+    delta_x = dfpetsc.create_vector_block(residual_comp)
+
+    offset = V.dofmap.index_map.size_local * V.dofmap.index_map_bs
+
+
+    ksp = PETSc.KSP().create(fluid_mesh.comm)
+    ksp.setOperators(A)
+    ksp.setType("preonly")
+    ksp.getPC().setType("lu")
+    ksp.getPC().setFactorSolverType("mumps")
+
+    max_iter = 20
+    atol = 1.0e-8
+    rtol = 1.0e-8
+
+
+    writer = dfx.io.VTXWriter(comm, "output/dfg_2d_3.bp", [v])
+    writer_p = dfx.io.VTXWriter(comm, "output/dfg_2d_3_p.bp", [p])
+
+    from timeit import default_timer as timer
+
+
+    # Set up computing drag and lift
+
+    class Drag:
+        def __init__(self, save_to: PathLike | list, mesh: dfx.mesh.Mesh, tags: int | tuple[int]):
+
+            self.save_to = save_to
+            self.comm = mesh.comm
+
+            self.ds = ufl.Measure("ds", domain=mesh, subdomain_data=fluid_facet_tags)(tags)
+            n = -ufl.FacetNormal(mesh)
+
+            form = ufl.inner(Fluid.NS_eulerian(v, p, nu_f, rho_f) * n, ufl.as_vector([-1.0, 0.0])) * self.ds
+            self.form = dfx.fem.form(form)
+
+            if not isinstance(self.save_to, list):
+                with open(self.save_to, "wb") as f:
+                    np.savetxt(f, [], header="time drag", fmt='%.4e', delimiter=' ')
+
+            return
+        
+        def __call__(self, t: float):
+            
+            drag = self.comm.reduce(dfx.fem.assemble_scalar(self.form), op=MPI.SUM, root=0)
+            if comm.rank == 0:
+                if isinstance(self.save_to, list):
+                    self.save_to.append([t, drag])
+                else:
+                    with open(self.save_to, "ab") as f:
+                        np.savetxt(f, [[t, drag]], fmt='%.4e', delimiter=' ')
+
+            return
+        
+    class Lift:
+        def __init__(self, save_to: PathLike | list, mesh: dfx.mesh.Mesh, tags: int | tuple[int]):
+
+            self.save_to = save_to
+            self.comm = mesh.comm
+
+            self.ds = ufl.Measure("ds", domain=mesh, subdomain_data=fluid_facet_tags)(tags)
+            n = -ufl.FacetNormal(mesh)
+
+            form = ufl.inner(Fluid.NS_eulerian(v, p, nu_f, rho_f) * n, ufl.as_vector([0.0, 1.0])) * self.ds
+            self.form = dfx.fem.form(form)
+
+            if comm.rank == 0 and not isinstance(self.save_to, list):
+                with open(self.save_to, "wb") as f:
+                    np.savetxt(f, [], header="time lift", fmt='%.4e', delimiter=' ')
+
+            return
+        
+        def __call__(self, t: float):
+            
+            lift = self.comm.reduce(dfx.fem.assemble_scalar(self.form), op=MPI.SUM, root=0)
+            if comm.rank == 0:
+                if isinstance(self.save_to, list):
+                    self.save_to.append([t, lift])
+                else:
+                    with open(self.save_to, "ab") as f:
+                        np.savetxt(f, [[t, lift]], fmt='%.4e', delimiter=' ')
+
+            return
+        
+    drag_hook = Drag("output/dfg_2d_3_drag.txt", fluid_mesh, PHYSICAL_MARKERS["obstacle"])
+    lift_hook = Lift("output/dfg_2d_3_lift.txt", fluid_mesh, PHYSICAL_MARKERS["obstacle"])
+    # drag_hook = Drag([], fluid_mesh, PHYSICAL_MARKERS["obstacle"])
+    # lift_hook = Lift([], fluid_mesh, PHYSICAL_MARKERS["obstacle"])
+
+    hooks = [drag_hook, lift_hook]
+
+    start = timer()
+
+    i = 0
+    t = t0
+    while t < T:
+
+        i += 1
+        t += dt.value
+        bc_func.interpolate(BCFunc(t))
+
+        v_old.x.array[:] = v.x.array
+        v_old.x.scatter_forward()
+
+        x.array[:offset] = v.x.array[:offset]
+        x.array[offset:] = p.x.array[:(len(x.array_r) - offset)]
+        x.ghostUpdate(addv=PETSc.InsertMode.INSERT_VALUES, mode=PETSc.ScatterMode.FORWARD)
+
+
+        if comm.rank == 0:
+            print(f"\n{t = :.3f}", end="\t")
+
+        n = 0
+        res0 = 1.0
+        while n < max_iter:
+
+
+            with b.localForm() as b_loc:
+                b_loc.set(0)
+
+            dfpetsc.assemble_vector_block(b, residual_comp, jacobian_comp, bcs=bcs, alpha=-1.0, x0=x)
+            b.ghostUpdate(PETSc.InsertMode.INSERT_VALUES, PETSc.ScatterMode.FORWARD)
+
+            res = b.norm()
+            if n == 0:
+                res0 = res
+                if comm.rank == 0:
+                    print(f"{res0 = :.3e}")
+
+            if comm.rank == 0:
+                print(f"{n = :2d}:\t\t{res  = :.3e}")
+
+            if res < atol or res < rtol * res0:
+                break
+
+            A.zeroEntries()
+            dfpetsc.assemble_matrix_block(A, jacobian_comp, bcs=bcs)
+            A.assemble()
+
+
+            ksp.solve(b, delta_x)
+
+            x.axpy(-1.0, delta_x)
+
+            v.x.array[:offset] = x.array_r[:offset]
+            p.x.array[: (len(x.array_r) - offset)] = x.array_r[offset:]
+            v.x.scatter_forward()
+            p.x.scatter_forward()
+
+            n += 1
+
+        if comm.rank == 0:
+            sys.stdout.flush()
+
+        if n == max_iter:
+            writer.close()
+            writer_p.close()
+            raise RuntimeError("Nonlinear solver did not converge")
+        
+        for hook in hooks:
+            hook(t)
+
+        if i % save_every == 0:
+            writer.write(t)
+            writer_p.write(t)
+
+    end = timer()
+    if comm.rank == 0:
+        print(f"\n# processes: {comm.size}")
+        print(f"Elapsed time: {end - start:.2f} s")
+        print(f"Time per step: {(end - start) / i:.3e} s")
+
+    A.destroy()
+    b.destroy()
+    x.destroy()
+    delta_x.destroy()
+    writer.close()
+
+
+    # dfx.common.list_timings(comm, [dfx.common.TimingType.wall])
+    # log.view()
+
+    if comm.rank == 0:
+        if isinstance(drag_hook.save_to, list):
+            drag_arr = np.array(drag_hook.save_to)
+        else:
+            drag_arr = np.loadtxt(drag_hook.save_to)
+        if isinstance(lift_hook.save_to, list):
+            lift_arr = np.array(lift_hook.save_to)
+        else:
+            lift_arr = np.loadtxt(lift_hook.save_to)
+        
+        plt.figure()
+        plt.plot(drag_arr[:, 0], drag_arr[:, 1], 'k-')
+        plt.xlabel("Time")
+        plt.ylabel("Drag")
+        plt.savefig("output/dfg_2d_3_drag.png")
+
+        plt.figure()
+        plt.plot(lift_arr[:, 0], lift_arr[:, 1], 'k-')
+        plt.xlabel("Time")
+        plt.ylabel("Lift")
+        plt.savefig("output/dfg_2d_3_lift.png")
+
+
+
+    return
+
+
+if __name__ == "__main__":
+    main()
